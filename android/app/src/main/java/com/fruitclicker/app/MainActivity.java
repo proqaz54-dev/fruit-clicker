8dcd88eeab2efabd31e069e27426c4508aff26c5
proqaz54-dev <proqaz54-dev@example.com>
2026-07-24 18:53:11 +0000

    fix: simplify Android build - remove icon requirement, use plain Activity, minimal deps

--- android/app/src/main/java/com/fruitclicker/app/MainActivity.java
+++ android/app/src/main/java/com/fruitclicker/app/MainActivity.java
@@ -1,22 +1,21 @@
 package com.fruitclicker.app;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        webView = findViewById(R.id.webView);
+        webView = new WebView(this);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
@@ -26,11 +25,11 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setUseWideViewPort(true);
         webSettings.setBuiltInZoomControls(false);
         webSettings.setDisplayZoomControls(false);
-        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
 
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
 
+        setContentView(webView);
         webView.loadUrl("file:///android_asset/index.html");
     }
 
